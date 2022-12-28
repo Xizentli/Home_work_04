c54aa270b073c2c8f9e33dd5781b0b955c78e102
Galina <akaides@yandex.ru>
2022-12-29 02:49:59 +1000

    Add task 002

---
+++ Task002/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+    while(!isCorrect)
+    {
+        Console.Write(message);
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ошибка ввода: введено не число.");
+        }
+    }
+    return result;
+}
+
+int PrintDigitCount(int number)
+{
+    int sum = 0; //сумма цифр
+    int raz = 0; //переменная для разряда числа
+
+    while(number > 0 || number < 0)
+    {
+        raz = number % 10;
+        number = number / 10;
+        sum = sum + raz;
+    }
+    return sum;
+}
+
+int number = GetNumber("Введите число: ");
+int sum = PrintDigitCount(number);
+Console.WriteLine($"Сумма цифр в числе: {sum}");
